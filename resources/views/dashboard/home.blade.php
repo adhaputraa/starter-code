b569fa447ec3a89127840a4eac537189ac5d7e6e
Achmad Adha Putra <adhaputraa@gmail.com>
2024-01-20 22:52:28 +0700

    Update Blade Components Starter Code

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -5,10 +5,16 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>Blank Page</h1>
+            <h1>Homepage</h1>
         </div>
 
         <div class="section-body">
         </div>
     </section>
+@endsection
+
+@section('sidebar')
+    @parent
+    <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i>
+            <span>Blank Page</span></a></li>
 @endsection
